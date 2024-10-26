860a11dbdb2231311537f0579911fd6838357496
Marcio Bueno <marciobueno@gmail.com>
2024-10-26 00:48:09 +0000

    Métodos selection sort e bubble sort

--- exemplo08/src/Main.java
+++ exemplo08/src/Main.java
@@ -2,11 +2,13 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int[] v = new int[10];
+        int[] v = new int[30];
         preencher(v);
         System.out.println("Antes da ordenação");
         imprimir(v);
-        insertionSort(v, v.length);
+        // insertionSort(v, v.length);
+        selectionSort(v, v.length);
+        bubbleSort(v, v.length);
         System.out.println("Após a ordenação");
         imprimir(v);
     }
@@ -27,6 +29,38 @@ public class Main {
                 j = j - 1;
             }
             v[j + 1] = tmp;
+        }
+    }
+
+    public static void selectionSort(int[] v, int tam) {	
+    	for (int i = 0; i < tam - 1; i++) {	
+            int i_menor = i;
+            for (int j = i + 1; j < tam; j++) {
+                if (v[j] < v[i_menor]) {
+                    i_menor = j;
+                }
+            }
+            int aux = v[i];
+            v[i] = v[i_menor];
+            v[i_menor] = aux;
+        }		
+    }
+
+    public static void bubbleSort (int[] v, int tam) {
+        int fim = tam - 2, pos = 0;
+        boolean trocou = true;
+        while (trocou) {
+            trocou = false;
+            for (int i = 0; i <= fim; i++) {
+                if (v[i] > v[i+1]) {
+                    int aux = v[i];
+                    v[i] = v[i + 1];
+                    v[i+1] = aux;
+                    pos = i;
+                    trocou = true;
+                }
+            }
+            fim = pos-1;
         }
     }
 
